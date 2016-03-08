bbe678d0dd21bd91879bf8408397bb06259c6f91
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2016-03-07 20:21:12 -0500

    Initial production of the desired output files
    These file contents need to be manually processed into the appropriate inputs for Quartus

--- create_template_snippets.py
+++ create_template_snippets.py
@@ -13,7 +13,15 @@ HDL_NAME_KEY = 'HDL Name'
 
 # String constant for generating Quartus Setting File snippet
 PIN_ASSIGNMENT_TEMPLATE = '''set_location_assignment {pin} -to {signal}
-set_instance_assignment -name IO_STANDARD "{standard}" -to {signal}'''
+set_instance_assignment -name IO_STANDARD "{standard}" -to {signal}
+'''
+MODULE_HEADER_TEMPLATE = '''{direction} {name};
+'''
+
+# Constants for filename manipulation
+CSV_EXTENSION_LOWER = '.csv'
+ASSIGNMENTS_SUFFIX = '-QSF_ASSIGNMENTS_OUT.txt'
+HEADERS_SUFFIX = '-VERILOG_HEADER_OUT.txt'
 
 def main():
   # Usage: `python create_template_snippets.py "Source Data.csv"`
@@ -31,6 +39,9 @@ def main():
 
   source_path = os.path.abspath(source_path_str)
 
+  assignments = []
+  headers = []
+
   with open(source_path, 'rU') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
@@ -38,7 +49,32 @@ def main():
         pin = row[DEVICE_PIN_KEY],
         signal = row[HDL_NAME_KEY],
         standard = row[IO_STANDARD_KEY])
-      print(assignment)
+      header = MODULE_HEADER_TEMPLATE.format(
+        direction = row[PIN_DIRECTION_KEY],
+        name = row[HDL_NAME_KEY])
+
+      assignments.append(assignment)
+      headers.append(header)
+
+  output_base_path_str = source_path_str
+
+  if output_base_path_str.lower().endswith(CSV_EXTENSION_LOWER):
+    output_base_path_str = output_base_path_str[:-4]
+
+  output_assignments_path_str = output_base_path_str + ASSIGNMENTS_SUFFIX
+  output_headers_path_str = output_base_path_str + HEADERS_SUFFIX
+
+  print('Clobbering ' + output_assignments_path_str)
+  output_assignments_path = os.path.abspath(output_assignments_path_str)
+
+  with open(output_assignments_path, 'w') as output:
+    output.writelines(assignments)
+
+  print('Clobbering ' + output_headers_path_str)
+  output_headers_path = os.path.abspath(output_headers_path_str)
+
+  with open(output_headers_path, 'w') as output:
+    output.writelines(headers)
 
 # Call the main() function to begin the program.
 if __name__ == "__main__":
